5621e083db9bfddab248c5258ac0cf42df32f29d
matthewogtong <ogtongm@gmail.com>
2021-11-17 09:08:35 -0500

    Enable viewBinding

--- app/src/main/java/com/matthewogtong/recipenotes/MainActivity.kt
+++ app/src/main/java/com/matthewogtong/recipenotes/MainActivity.kt
@@ -2,10 +2,17 @@ package com.matthewogtong.recipenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recipesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recipesRV = find
     }
 }
